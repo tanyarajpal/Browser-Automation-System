53a31fa30601acc70c94051e4c3c5c4c9d11fdf4
Tanya933 <tanya.rajpal0400@gmail.com>
2022-12-13 10:32:56 +0530

    first commit

--- controllers/actions.js
+++ controllers/actions.js
@@ -2,24 +2,19 @@ const { exec } = require("child_process");
 
 
 exports.openURL = async (req,res)=>{
-    console.log("here")
     try{
         const url = req.query.url;
         const browser = req.query.browser;
-        console.log(url);
-        console.log(browser);
         exec(`open -a "${browser}" ${url}`)
         // await open('http://'+url, {app: {name:browser}});
         res.send("url opened in "+ browser+" app");
     }catch(err){
         res.send("error opening url" , err);
-        console.log(err)
     }
 }
 
 exports.clearCacheAndHistory = async(req,res)=>{
     try{
-        console.log(req);
         res.send({message:"history cleared "});
     }catch(err){
         res.json({message:"error clearing history ", err});
